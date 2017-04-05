5304d4550e1a2aa534c5f675b7d811d8f4fffedc
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-05 22:07:13 +0000

    create main index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions unless test?
-    set :session_secret, "secret"
+  set :public_folder, 'public'
+  set :views, 'app/views'
+  enable :sessions unless test?
+  set :session_secret, "secret"
+
+  get '/' do
+    'hello world'
   end
 
 end
